2924f06a0e84f7357afa761da267ac31fc1bce47
jonathandry32 <119029850+jonathandry32@users.noreply.github.com>
2024-02-01 16:59:03 +0300

    Update ModeleRepository.java

--- src/main/java/com/vehicule/api/repository/ModeleRepository.java
+++ src/main/java/com/vehicule/api/repository/ModeleRepository.java
@@ -4,4 +4,5 @@ import com.vehicule.api.entity.Modele;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ModeleRepository extends JpaRepository<Modele, Long> {
+  List<Modele> findAllByMarqueIdMarque(int idMarque);
 }
